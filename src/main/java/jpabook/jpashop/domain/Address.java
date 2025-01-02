81add04ad339718fc95e906a5efcdada2245a7f6
Park Hyewon <db1648@naver.com>
2025-01-02 20:16:50 +0900

    240102 엔티티 클래스 개발2

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -10,4 +10,12 @@ public class Address {
     private String street;
     private String city;
     private String zipcode;
+
+    protected Address() {
+    }
+    public Address(String street, String city, String zipcode) {
+        this.street = street;
+        this.city = city;
+        this.zipcode = zipcode;
+    }
 }
