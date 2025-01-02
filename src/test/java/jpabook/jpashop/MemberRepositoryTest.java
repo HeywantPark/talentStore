eac608de8d52253bba2ca8a96bf6dd467f67923b
Park Hyewon <db1648@naver.com>
2025-01-02 18:48:44 +0900

    250102 엔티티 클래스 개발1

---
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,32 @@
+package jpabook.jpashop;
+
+import jpabook.jpashop.domain.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@SpringBootTest
+public class MemberRepositoryTest {
+
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    public void testMember() throws Exception {
+        //given
+        Member member = new Member();
+        member.setUsername("memberA");
+
+        //when
+        Long saveId = memberRepository.save(member);
+        Member member1 = memberRepository.find(saveId);
+
+        //then
+        Assertions.assertThat(member1.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(member1.getUsername()).isEqualTo(member.getUsername());
+    }
+
+}
